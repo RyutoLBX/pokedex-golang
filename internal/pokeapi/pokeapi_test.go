0114c748d2264d6ff4e05cbb2c2a3f808b084368
RyutoLBX <37038835+RyutoLBX@users.noreply.github.com>
2025-07-29 05:48:20 +0700

    test(pokeapi): add client timeout check

--- internal/pokeapi/pokeapi_test.go
+++ internal/pokeapi/pokeapi_test.go
@@ -1,19 +1,31 @@
 package pokeapi
 
 import (
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 )
 
 func TestClient(t *testing.T) {
-	const baseTime = 1 * time.Second
-	const waitTime = baseTime + 1*time.Second
-	const testURL = baseURL + "/location-area"
+	const timeout = 1 * time.Second
+	const responseTimeSlow = timeout + 1*time.Second
 
-	client := NewClient(baseTime)
-	req1, _ := http.NewRequest("GET", testURL, nil)
-	req2, _ := http.NewRequest("GET", testURL, nil)
+	server1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello World!")
+	}))
+	defer server1.Close()
+
+	server2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(responseTimeSlow) // simulate slow server
+		fmt.Fprintln(w, "Hello World.")
+	}))
+	defer server2.Close()
+
+	client := NewClient(timeout)
+	req1, _ := http.NewRequest("GET", server1.URL, nil)
+	req2, _ := http.NewRequest("GET", server2.URL, nil)
 
 	_, err := client.httpClient.Do(req1)
 	if err != nil {
@@ -21,7 +33,6 @@ func TestClient(t *testing.T) {
 		return
 	}
 
-	time.Sleep(waitTime)
 	_, err = client.httpClient.Do(req2)
 	if err == nil {
 		t.Errorf("expected to timeout client")
